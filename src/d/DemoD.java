631f8949c27d635ac299d83779e9cebed7e07078
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-03 21:16:40 -0500

    fourth commitment

---
+++ src/d/DemoD.java
@@ -0,0 +1,14 @@
+package d;
+
+public class DemoD {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is class D");
+		
+		System.out.println("we're getting there");
+		System.out.println("maybe I got this");
+		
+	}
+
+}
